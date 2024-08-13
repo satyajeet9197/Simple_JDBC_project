ce6d7cc2c5a4b43f27a7bf7429b39b3d2c8084e3
satyajeet9197 <satyajeet9197@gmail.com>
2024-08-13 19:57:12 +0530

    Add files via upload

---
+++ User.java
@@ -0,0 +1,63 @@
+package edu.jsp.entity;
+
+import java.sql.Date;
+
+public class User {
+	private int userid;
+	private String firstname;
+	private String lastname;
+	private long mobile;
+	private String email;
+	private String password;
+	private Date dob;
+	public int getUserid() {
+		return userid;
+	}
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public long getMobile() {
+		return mobile;
+	}
+	public void setMobile(long mobile) {
+		this.mobile = mobile;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Date getDob() {
+		return dob;
+	}
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+	@Override
+	public String toString() {
+		return "User [userid=" + userid + ", firstname=" + firstname + ", lastname=" + lastname + ", mobile=" + mobile
+				+ ", email=" + email + ", password=" + password + ", dob=" + dob + "]";
+	}
+	
+	
+
+}
